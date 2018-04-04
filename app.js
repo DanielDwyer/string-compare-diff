811ed0baf95b1b72bf105a45efe946ea6233cc7f
DanielDwyer <danielpatrickdwyer@gmail.com>
2018-04-04 08:18:08 -0600

    fixing export errors

--- app.js
+++ app.js
@@ -5,19 +5,20 @@
 ** example console.log(strSimilarity("Daniel", "Dan"))
 **
 */
-
-const strSimilarity = (s1, s2) => {
-  var longer = s1
-  var shorter = s2
-  if (s1.length < s2.length) {
-    longer = s2
-    shorter = s1
+module.exports = {
+  strSimilarity: function(s1, s2){
+    var longer = s1
+    var shorter = s2
+    if (s1.length < s2.length) {
+      longer = s2
+      shorter = s1
+    }
+    var longerLength = longer.length
+    if (longerLength == 0) {
+      return 1.0
+    }
+    return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength)
   }
-  var longerLength = longer.length
-  if (longerLength == 0) {
-    return 1.0
-  }
-  return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength)
 }
 
 const editDistance = (s1, s2) => {
